b23ff602a95c1c0a5b8ebcadca2043eeff07cc93
minlx <1158778004@qq.com>
2017-10-05 19:55:36 +0800

    more

---
+++ js/advertising.js
@@ -0,0 +1,4 @@
+/**
+ * Created by lenovo on 2017/9/28.
+ */
+document.documentElement.style.fontSize = document.documentElement.clientWidth/375*50+"px";
---
+++ js/more.js
@@ -0,0 +1,7 @@
+/**
+ * Created by lenovo on 2017/9/28.
+ */
+document.documentElement.style.fontSize = document.documentElement.clientWidth/375*50+"px";
+var myiscroller = new IScroll('.section',{
+    mouseWheel:true
+});
